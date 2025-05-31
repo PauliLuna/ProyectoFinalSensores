9b35013132ec6da6e9e9aaa749fc049fa30ec01b
MSpitt <marspitt54@gmail.com>
2025-05-31 16:18:42 -0300

    added logic to calculate lat and long from the address, city, province and country

--- backend/controllers/sensor_controller.py
+++ backend/controllers/sensor_controller.py
@@ -1,10 +1,33 @@
 from flask import request, jsonify
 from models.sensor import insert_sensor
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
+
+def get_coordinates_from_address(address):
+    """
+    Convierte una dirección en coordenadas (latitud, longitud) usando Nominatim.
+    """
+    geolocator = Nominatim(user_agent="sensor_app")
+    try:
+        location = geolocator.geocode(address)
+        if location:
+            return location.latitude, location.longitude
+        else:
+            return None, None
+    except GeocoderTimedOut:
+        return None, None
+
 
 def register_sensor(mongo):
     """
-    Lee los datos del formulario (por POST) y los inserta en la base de datos.
+    Lee los datos del formulario (por POST),convierte la dirección en coordenadas y los inserta en la base de datos.
     """
+
+     # Construimos la dirección completa
+    full_address = f"{request.form.get('direccion')}, {request.form.get('ciudad')}, {request.form.get('provincia')}, {request.form.get('pais')}"
+
+    lat, lon = get_coordinates_from_address(full_address)
+
     sensor_data = {
          "nroSensor": request.form.get('nroSensor'),
          "alias": request.form.get('alias'),
@@ -15,7 +38,9 @@ def register_sensor(mongo):
          "provincia": request.form.get('provincia'),
          "ciudad": request.form.get('ciudad'),
          "cp": request.form.get('cp'),
-         "estado": request.form.get('estado')
+         "estado": request.form.get('estado'), 
+         "latitud": lat,
+         "longitud": lon
     }
     # Puedes agregar validaciones aquí según lo necesario
     sensor_id = insert_sensor(mongo, sensor_data)
