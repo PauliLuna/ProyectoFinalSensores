fb7814202778255892ca40503ab8376d805cd9a5
Loriana-Boixadera <lorianaboixadera@gmail.com>
2025-06-09 13:04:39 -0300

    Delete unused variables

--- backend/controllers/empresa_controller.py
+++ backend/controllers/empresa_controller.py
@@ -7,11 +7,7 @@ def register_empresa(mongo):
     Registra un nuevo empresa en la base de datos.
     Se guardan los datos de la empresa.
     """
-    now = datetime.datetime.now()
-    fechaAlta = now.strftime("%d/%m/%Y")
-    # Por el empresa, generalmente la fecha de alta se registra solo en la parte de usuario,
-    # pero puedes agregarla si es necesario en este caso.
-    
+
     empresa_data = {
         "idCode": request.form.get('idCode'),
         "companyName": request.form.get('companyName'),
@@ -21,7 +17,7 @@ def register_empresa(mongo):
         "provincia": request.form.get('provincia'),
         "ciudad": request.form.get('ciudad'),
         "cp": request.form.get('cp')
-        # Agrega otros campos propios de la empresa si los necesitas
     }
+
     empresa_id = insert_empresa(mongo, empresa_data)
     return jsonify({"message": "empresa registrado correctamente", "empresa_id": empresa_id}), 201
