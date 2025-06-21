aeae8d41946325c9c86beaa1224d5956e26e4d01
MSpitt <marspitt54@gmail.com>
2025-06-21 17:31:59 -0300

    Redisigned myprofile.html form and connected it to Usuarios DB

--- backend/routes/usuario_routes.py
+++ backend/routes/usuario_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, current_app, jsonify, session
+from flask import Blueprint, current_app, jsonify, session, request
 from controllers.usuario_controller import register_usuario, invite_user, login_usuario, complete_registration, get_ultimas_conexiones
 from bson import ObjectId
 
@@ -15,24 +15,43 @@ def get_usuarios():
         usuario["_id"] = str(usuario["_id"])
     return jsonify(usuarios)
 
-@usuario_bp.route('/usuario_actual', methods=['GET'])
+@usuario_bp.route('/ultimas_conexiones', methods=['GET'])
+def ultimas_conexiones_route():
+    mongo = current_app.mongo
+    return get_ultimas_conexiones(mongo)
+
+@usuario_bp.route('/usuario_actual', methods=['GET', 'PUT'])
 def usuario_actual_route():
     mongo = current_app.mongo
     user_id = session.get('user_id')
     if not user_id:
         return jsonify({"error": "No autorizado"}), 401
+
     usuario = mongo.db.usuarios.find_one({"_id": ObjectId(user_id)})
     if not usuario:
         return jsonify({"error": "Usuario no encontrado"}), 404
-    return jsonify({
-        "username": usuario.get("username", ""),
-        "roles": usuario.get("roles", "")
-    })
 
-@usuario_bp.route('/ultimas_conexiones', methods=['GET'])
-def ultimas_conexiones_route():
-    mongo = current_app.mongo
-    return get_ultimas_conexiones(mongo)
+    if request.method == 'GET':
+        return jsonify({
+            "email": usuario.get("email", ""),
+            "username": usuario.get("username", ""),
+            "phone": usuario.get("phone", ""),
+            "roles": usuario.get("roles", "")
+        })
+    elif request.method == 'PUT':
+        data = request.get_json()
+        update_fields = {
+            "username": data.get("username", usuario.get("username", "")),
+            "phone": data.get("phone", usuario.get("phone", ""))
+        }
+        if data.get("password"):
+            from werkzeug.security import generate_password_hash
+            update_fields["password"] = generate_password_hash(data["password"], method='pbkdf2:sha256')
+        mongo.db.usuarios.update_one(
+            {"_id": ObjectId(user_id)},
+            {"$set": update_fields}
+        )
+        return jsonify({"message": "Perfil actualizado correctamente"})
 
 @usuario_bp.route('/usuario', methods=['POST'])
 def register_usuario_route():
